5569514fa7de6a8336cce8b56e6387c8ba8e939c
josedig <josedanieliguarangonzalez@gmail.com>
2025-10-06 15:36:15 -0400

    Update #Curso ejercico 1 y 2.py

--- #Curso ejercico 1 y 2.py
+++ #Curso ejercico 1 y 2.py
@@ -7,3 +7,19 @@ edad = 21
 
 print(f"Hola, mi nombre es {nombre} y mi apellio es {apellido} y tengo {edad} años de edad.")
 
+# ejercicio 2
+
+Numero1 = int(input("cual es el primer numero?  "))
+Numero2 = int(input("cual el el segundo numero? "))
+
+suma = Numero1 + Numero2
+resta = Numero1 - Numero2
+multiplicacion = Numero1 * Numero2
+division = Numero1 / Numero2
+
+print (f"la suma es : {suma}")
+print (f"la resta es : {resta}")
+print (f"la multipliucacion es: {multiplicacion}")
+print (f"la division es: {division}")
+
+
