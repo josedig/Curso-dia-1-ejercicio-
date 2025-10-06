c290e2c5cc0e292f5d4986caec3d54be60c7c1a2
josedig <josedanieliguarangonzalez@gmail.com>
2025-10-06 11:47:29 -0400

    Add files via upload

---
+++ #Curso ejercico 1 y 2.py
@@ -0,0 +1,9 @@
+#Curso ejercico 1
+nombre =  "jose"                             
+apellido = "iguaran"
+edad = 21
+
+#f-string
+
+print(f"Hola, mi nombre es {nombre} y mi apellio es {apellido} y tengo {edad} años de edad.")
+
