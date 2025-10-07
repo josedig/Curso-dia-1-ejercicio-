638c1a26f88ed2475369e7eb7e18308479e60b82
josedig <josedanieliguarangonzalez@gmail.com>
2025-10-07 12:01:47 -0400

    Add files via upload

---
+++ ejercicio 8.py
@@ -0,0 +1,12 @@
+#ejercicio 8
+
+mi_diccionario = {
+    "nombre" : "jose",
+    "edad" : "21",
+    "cuidad:" : "maracaibo", 
+    "cedula:" : "30862844",
+    "sexo:" : "masculino",
+}
+
+for clave, valor in mi_diccionario.items():
+    print (f"\n {clave}: {valor}")
