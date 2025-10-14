5feaefb0f25170a297564404c0c3d6fcca2c2a14
josedig <josedanieliguarangonzalez@gmail.com>
2025-10-14 12:01:56 -0400

    Add files via upload

---
+++ examen/practicas/variables.js
@@ -0,0 +1,8 @@
+let nombre = "jose";
+console.log("nombre:", nombre);
+
+let apellido = "iguara";
+console.log("apellido:", apellido);
+
+let nacimiento = "31-08-2004";
+console.log("nacimiento:", nacimiento);
